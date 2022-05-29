52432568e8d4e3eb25f3e321cdba78f8ac1a38ae
Ilyas Adiyasa <ilyasadiyasa4@gmail.com>
2022-05-29 15:33:40 +0700

    add shop page, add some styling on product-card

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import reportWebVitals from './reportWebVitals';
 
 import App from './App';
 import { UserProvider } from './context/user.context';
+import { ProductsProvider } from './context/products.context';
 
 import './index.scss';
 
@@ -13,7 +14,9 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
       <UserProvider>
-        <App />
+        <ProductsProvider>
+          <App />
+        </ProductsProvider>
       </UserProvider>
     </BrowserRouter>
   </React.StrictMode>
