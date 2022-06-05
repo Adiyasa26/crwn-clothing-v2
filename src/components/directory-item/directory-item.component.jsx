b2928ffbd1cc8e9c6b7400e8c87a1552077c42de
Ilyas Adiyasa <ilyasadiyasa4@gmail.com>
2022-06-05 17:57:46 +0700

    Clean up Directory component and Home component, and add functionality in directory component

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,9 +1,15 @@
+import { useNavigate } from 'react-router-dom'
+
 import { DirectoryItemContainer, Body, BackgroundImage } from './directory-item.styles';
 
 const DirectoryItem = ({ category }) => {
-  const { title, imageUrl } = category;
+  const { title, imageUrl, route } = category;
+  const navigate = useNavigate()
+
+  const onNavigateHandler = () => navigate(route)
+
   return (
-    <DirectoryItemContainer>
+    <DirectoryItemContainer onClick={onNavigateHandler}>
       <BackgroundImage
         imageUrl = {imageUrl}
       />
