b020c8d09218537c708e06e3498a15746b089004
Ilyas Adiyasa <ilyasadiyasa4@gmail.com>
2022-06-05 16:30:54 +0700

    Replace Cart Icon, Dropdown, and Directory Item styling from scss to styled components

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react'
+import { useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import { CartContext } from '../../context/cart.context';
@@ -6,26 +6,32 @@ import { CartContext } from '../../context/cart.context';
 import Button from '../button/button.component';
 import CartItem from '../cart-item/cart-item.component';
 
-import './cart-dropdown.styles.scss'
+import {
+  DropdownContainer,
+  EmptyMessage,
+  CartItems,
+} from './cart-dropdown.styles';
 
 const CartDropdown = () => {
-    const { cartItems } = useContext(CartContext)
-    const navigate = useNavigate()
+  const { cartItems } = useContext(CartContext);
+  const navigate = useNavigate();
 
-    const goToCheckoutHandler = () => {
-        navigate('/checkout')
-      }
+  const goToCheckoutHandler = () => {
+    navigate('/checkout');
+  };
 
-    return (
-        <div className="cart-dropdown-container">
-            <div className="cart-items">
-                {cartItems.map((item) => (
-                    <CartItem key={item.id} cartItem={item}/>
-                ))}
-            </div>
-            <Button onClick={goToCheckoutHandler}>GO TO CHECKOUT</Button>
-        </div>
-    )
-}
+  return (
+    <DropdownContainer>
+      <CartItems>
+        {cartItems.length ? (
+          cartItems.map(item => <CartItem key={item.id} cartItem={item} />)
+        ) : (
+          <EmptyMessage>Your cart is empty</EmptyMessage>
+        )}
+      </CartItems>
+      <Button onClick={goToCheckoutHandler}>GO TO CHECKOUT</Button>
+    </DropdownContainer>
+  );
+};
 
 export default CartDropdown;
--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,20 +1,17 @@
-import './directory-item.styles.scss';
+import { DirectoryItemContainer, Body, BackgroundImage } from './directory-item.styles';
 
 const DirectoryItem = ({ category }) => {
   const { title, imageUrl } = category;
   return (
-    <div className="directory-item-container">
-      <div
-        className="background-image"
-        style={{
-          backgroundImage: `url(${imageUrl})`,
-        }}
+    <DirectoryItemContainer>
+      <BackgroundImage
+        imageUrl = {imageUrl}
       />
-      <div className="body">
+      <Body>
         <h2>{title}</h2>
         <p>Shop Now</p>
-      </div>
-    </div>
+      </Body>
+    </DirectoryItemContainer>
   );
 };
 
