e679965e55a1ce110101440f7c2e9f06bd909c48
Ilyas Adiyasa <ilyasadiyasa4@gmail.com>
2022-06-05 11:17:13 +0700

    Replace Button Component styling from scss into styled-components

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -4,7 +4,7 @@ import './product-card.styles.scss';
 
 import { CartContext } from '../../context/cart.context';
 
-import Button from '../button/button.component';
+import Button, { BUTTON_TYPE_CLASSES } from '../button/button.component';
 
 const ProductCard = ({ product }) => {
   const { name, price, imageUrl } = product;
@@ -19,7 +19,7 @@ const ProductCard = ({ product }) => {
         <span className="name">{name}</span>
         <span className="price">{price}</span>
       </div>
-      <Button buttonType="inverted" onClick={addProductToCart}>
+      <Button buttonType={BUTTON_TYPE_CLASSES.inverted} onClick={addProductToCart}>
         Add to Cart
       </Button>
     </div>
--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -6,7 +6,7 @@ import {
   signInUserAuthWithEmailAndPassword,
 } from '../../utils/firebase/firebase.utils';
 
-import Button from '../button/button.component';
+import Button, { BUTTON_TYPE_CLASSES } from '../button/button.component';
 import FormInput from '../form-input/form-input.component';
 
 import './sign-in-form.styles.scss';
@@ -74,7 +74,7 @@ const SignInForm = () => {
 
         <div className="buttons-container">
           <Button type="submit">Sign In</Button>
-          <Button type="button" buttonType="google" onClick={SignInWithGoogle}>
+          <Button type="button" buttonType={BUTTON_TYPE_CLASSES.google} onClick={SignInWithGoogle}>
             Google sign In
           </Button>
         </div>
