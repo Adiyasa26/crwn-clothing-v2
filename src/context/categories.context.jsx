569e84919099051e7cdc7ff33c6a6b5f1368176e
Ilyas Adiyasa <ilyasadiyasa4@gmail.com>
2022-06-07 16:14:21 +0700

    Add reducer in project

--- src/context/categories.context.jsx
+++ src/context/categories.context.jsx
@@ -1,13 +1,40 @@
-import { createContext, useState, useEffect } from 'react';
+import { createContext, useEffect, useReducer } from 'react';
 
 import { getCategoriesAndDocuments } from '../utils/firebase/firebase.utils.js';
+import { createAction } from '../utils/reducer/reducer.utils';
 
 export const CategoriesContext = createContext({
   categoriesMap: {},
 });
 
+const CATEGORIES_ACTION_TYPES = {
+  SET_CATEGORIES_MAP: 'SET_CATEGORIES_MAP'
+}
+
+const INITIAL_STATE = {
+  categoriesMap: {},
+}
+
+const categoriesReducer = ( state, action ) => {
+  const { type, payload } = action
+
+  switch(type) {
+    case CATEGORIES_ACTION_TYPES.SET_CATEGORIES_MAP:
+      return {
+        ...state,
+        categoriesMap: payload
+      }
+    default:
+      throw new Error(`Unhandled type ${type} in category reducer.`)
+  }
+}
+
 export const CategoriesProvider = ({ children }) => {
-  const [categoriesMap, setCategoriesMap] = useState({});
+  const [ {categoriesMap}, dispatch ] = useReducer(categoriesReducer, INITIAL_STATE)
+
+  const setCategoriesMap = (categoriesMap) => {
+    dispatch(createAction(CATEGORIES_ACTION_TYPES.SET_CATEGORIES_MAP, categoriesMap))
+  }
 
   useEffect(() => {
     const getCategoriesMap = async () => {
--- src/context/user.context.jsx
+++ src/context/user.context.jsx
@@ -1,6 +1,7 @@
 import { createContext, useEffect, useReducer } from 'react'
 
 import { createUserDocumentFromAuth, onAuthStateChangedListener } from '../utils/firebase/firebase.utils';
+import { createAction } from '../utils/reducer/reducer.utils';
 
 export const UserContext = createContext({
     currentUser: null,
@@ -11,13 +12,17 @@ export const USER_ACTION_TYPES = {
     SET_CURRENT_USER: 'SET_CURRENT_USER'
 }
 
+const INITIAL_STATE = {
+    currentUser: null,
+}
+
 const userReducer = (state,action) => {
     const { type, payload } = action
     
     switch(type) {
         case USER_ACTION_TYPES.SET_CURRENT_USER:
             return {
-                ... state,
+                ...state,
                 currentUser: payload
             }
         default:
@@ -25,15 +30,11 @@ const userReducer = (state,action) => {
     }
 }
 
-const INITIAL_STATE = {
-    currentUser: null,
-}
-
 export const UserProvider =  ({children}) => {
     const [ { currentUser }, dispatch ] = useReducer(userReducer, INITIAL_STATE)
 
     const setCurrentUser = (user) => {
-        dispatch({ type: USER_ACTION_TYPES.SET_CURRENT_USER, payload: user })
+        dispatch(createAction(USER_ACTION_TYPES.SET_CURRENT_USER, user))
     }
 
     const value = { currentUser, setCurrentUser };
