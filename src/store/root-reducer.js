b0a19ccc66de25313127e553ba03f21d59c9acad
Ilyas Adiyasa <ilyasadiyasa4@gmail.com>
2022-10-20 23:11:17 +0700

    add category redux

--- src/store/root-reducer.js
+++ src/store/root-reducer.js
@@ -1,6 +1,8 @@
-import { combineReducers } from 'redux'
-import { userReducer } from './user/user.reducer'
+import { combineReducers } from 'redux';
+import { userReducer } from './user/user.reducer';
+import { categoriesReducer } from './categories/category.reducer';
 
 export const rootReducers = combineReducers({
-    user: userReducer
-})
+  user: userReducer,
+  categories: categoriesReducer,
+});
