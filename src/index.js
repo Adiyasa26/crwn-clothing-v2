33539aaf3b5cebb3a72a01525fedc2d822eee621
Ilyas Adiyasa <ilyasadiyasa4@gmail.com>
2022-10-20 23:11:32 +0700

    remove categories provider in index js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 
 import App from './App';
-import { CategoriesProvider } from './context/categories.context';
 import { CartProvider } from './context/cart.context';
 import { store } from './store/store';
 
@@ -16,11 +15,9 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <CategoriesProvider>
-          <CartProvider>
-            <App />
-          </CartProvider>
-        </CategoriesProvider>
+        <CartProvider>
+          <App />
+        </CartProvider>
       </BrowserRouter>
     </Provider>
   </React.StrictMode>
