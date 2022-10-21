c636cffb5acea5bd57e92b98bb900d73e7cbc9aa
Ilyas Adiyasa <ilyasadiyasa4@gmail.com>
2022-10-21 17:42:43 +0700

    hide firebase config

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -18,15 +18,7 @@ import {
   query,
   getDocs,
 } from 'firebase/firestore';
-
-const firebaseConfig = {
-  apiKey: 'AIzaSyB3u60wFOuvPe-oLLEeUX1SuJbhFq2_kgA',
-  authDomain: 'crwn-clothing-v2-91ba7.firebaseapp.com',
-  projectId: 'crwn-clothing-v2-91ba7',
-  storageBucket: 'crwn-clothing-v2-91ba7.appspot.com',
-  messagingSenderId: '527455137734',
-  appId: '1:527455137734:web:967c0b4c34c36779ea0cc7',
-};
+import { firebaseConfig } from './firebase.init';
 
 initializeApp(firebaseConfig);
 
