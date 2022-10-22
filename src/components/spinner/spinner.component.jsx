9f550ae3e0ff517a82b3553865d913b64bef3b11
Ilyas Adiyasa <ilyasadiyasa4@gmail.com>
2022-10-22 19:21:11 +0700

    add spinner component and styling

---
+++ src/components/spinner/spinner.component.jsx
@@ -0,0 +1,11 @@
+import { SpinnerOverlay, SpinnerContainer } from './spinner.styles';
+
+const Spinner = () => {
+  return (
+    <SpinnerOverlay>
+      <SpinnerContainer />
+    </SpinnerOverlay>
+  );
+};
+
+export default Spinner;
